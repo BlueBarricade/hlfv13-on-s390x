1d3f3ddb6cba5c3042dad572e2b97f4ae3698264
skyclean <skyclean906@gmail.com>
2019-05-21 22:12:30 +0800

    update

--- fabric-dev-servers/_loader.sh
+++ fabric-dev-servers/_loader.sh
@@ -1,9 +1,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +27,8 @@ if [ "${HL_FABRIC_START_TIMEOUT}" ]; then
 fi
 
 if [ -z ${FABRIC_VERSION+x} ]; then
- echo "FABRIC_VERSION is unset, assuming hlfv12"
- export FABRIC_VERSION="hlfv12"
+ echo "FABRIC_VERSION is unset, assuming hlfv13"
+ export FABRIC_VERSION="hlfv13"
 else
  echo "FABRIC_VERSION is set to '$FABRIC_VERSION'"
 fi
--- fabric-dev-servers/fabric-scripts/hlfv13/downloadFabric.sh
+++ fabric-dev-servers/fabric-scripts/hlfv13/downloadFabric.sh
@@ -3,9 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,11 +27,11 @@ pullFabricImage() {
 
 # Pull Hyperledger Fabric base images.
 for imageName in peer orderer ccenv; do
-    pullFabricImage ${imageName} 1.2.1
+    pullFabricImage ${imageName} 1.3.0
 done
 
 # Pull Hyperledger Fabric CA images.
-pullFabricImage ca 1.2.1
+pullFabricImage ca 1.3.0
 
 # Pull Hyperledger third-party images.
-pullFabricImage couchdb 0.4.10
+pullFabricImage couchdb 0.4.13
